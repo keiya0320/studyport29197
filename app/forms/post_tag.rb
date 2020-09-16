ca9d347943ead91ec7294cc7d4f6af34647869a3
keiya0320 <keiya.man@icloud.com>
2020-09-16 19:05:22 +0900

    投稿にタイトルの追加

--- app/forms/post_tag.rb
+++ app/forms/post_tag.rb
@@ -3,16 +3,17 @@ class PostTag
 
 
 
-  attr_accessor :message, :name, :user_id
+  attr_accessor :title, :message, :name, :user_id
 
   with_options presence: true do
+    validates :title
     validates :message
     validates :name
     validates :user_id
   end
 
   def save
-      post = Post.new(message: message, user_id: user_id)
+      post = Post.new(title: title, message: message, user_id: user_id)
       post.save
       tag = Tag.where(name: name).first_or_initialize
       tag.save
