c3ac77582e4c01198f571a7a3f01522f09cc4902
keiya0320 <keiya.man@icloud.com>
2020-09-22 18:53:52 +0900

    マイページの実装
    マイページボタンの作成と、マイペーjへの遷移
    
    投稿を新しい順に表示している

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
 
+  resources :users, only: :show
+
   root to: "home#index"
 end
