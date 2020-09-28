cf8016c9380cca8ea208c859db5575da9a0801dc
keiya0320 <keiya.man@icloud.com>
2020-09-28 14:26:05 +0900

    update

--- app/models/grade.rb
+++
@@ -1,5 +0,0 @@
-# class Grade < ActiveHash::Base
-#   self.data = [
-#     {id: 1, name: '---'},{id: 2, name: '中学1年生'},{id: 3, name: '中学2年生'},{id: 4, name: '中学3年生'},{id: 5, name: '高校1年生'},{id: 6, name: '高校2年生'},{id: 7, name: '高校3年生'}
-#   ]
-# end
